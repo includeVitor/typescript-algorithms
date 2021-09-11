8619a1ea5a59890dda5ce17f4c158f006e539921
Vitor Hugo <vitorzcomputacao@gmail.com>
2021-09-11 16:27:07 -0300

    setup ts-jest

---
+++ src/data-structures/linked-list/__tests__/LinkedList.ts
@@ -0,0 +1,7 @@
+import { a } from '@data-structures/linked-list/LinkedList'
+
+describe('LinkedList', () => {
+    it('should be ok', () => {
+        expect(a).toBe('abc')
+    })
+})
---
+++ src/data-structures/linked-list/__tests__/LinkedListNode.ts
@@ -0,0 +1,7 @@
+import { value } from '@data-structures/linked-list/LinkedListNode'
+
+describe('LinkedList', () => {
+    it('should be ok', () => {
+        expect(value).toBe(100)
+    })
+})
