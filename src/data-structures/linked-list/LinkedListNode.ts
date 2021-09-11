935020ba025645c8465a8b12f9bac36366570499
Vitor Hugo <vitorzcomputacao@gmail.com>
2021-09-11 17:17:18 -0300

    add LinkedListNode

--- src/data-structures/linked-list/LinkedListNode.ts
+++ src/data-structures/linked-list/LinkedListNode.ts
@@ -1,1 +1,12 @@
-export const value = 100
+export default class LinkedListNode {
+    value: any
+    next: LinkedListNode | null
+    constructor(value: any, next: LinkedListNode | null = null) {
+        this.value = value
+        this.next = next
+    }
+
+    toString(callback: any) {
+        return callback ? callback(this.value) : `${this.value}`
+    }
+}
