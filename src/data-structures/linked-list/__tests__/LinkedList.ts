e875c9079fcd90a77badecd83a1639d486a85b2f
Vitor Hugo <vitorzcomputacao@gmail.com>
2021-09-11 23:01:37 -0300

    add new test case

--- src/data-structures/linked-list/__tests__/LinkedList.ts
+++ src/data-structures/linked-list/__tests__/LinkedList.ts
@@ -42,4 +42,41 @@ describe('LinkedList', () => {
         //Assert
         expect(linkedList.toString()).toBe('3,2,1')
     })
+
+    it('should be able to delete an node by value', () => {
+        const linkedList = new LinkedList()
+
+        expect(linkedList.delete(26)).toBeNull()
+
+        linkedList.append(25)
+        linkedList.append(50)
+        linkedList.append(75)
+        linkedList.append(100)
+        linkedList.append(125)
+
+        expect(linkedList.head?.toString()).toBe('25')
+        expect(linkedList.tail?.toString()).toBe('125')
+
+        const deletedNode = linkedList.delete(100)
+        expect(deletedNode?.value).toBe(100)
+        expect(linkedList.toString()).toBe('25,50,75,125')
+
+        linkedList.delete(25)
+        expect(linkedList.toString()).toBe('50,75,125')
+
+        linkedList.delete(75)
+        expect(linkedList.toString()).toBe('50,125')
+
+        expect(linkedList.head?.toString()).toBe('50')
+        expect(linkedList.tail?.toString()).toBe('125')
+
+        linkedList.delete(50)
+        expect(linkedList.toString()).toBe('125')
+
+        expect(linkedList.head?.toString()).toBe('125')
+        expect(linkedList.tail?.toString()).toBe('125')
+
+        linkedList.delete(125)
+        expect(linkedList.toString()).toBe('')
+    })
 })
--- src/data-structures/linked-list/__tests__/LinkedListNode.ts
+++
@@ -1,8 +0,0 @@
-// import { Value } from '@data-structures/linked-list/LinkedListNode'
-
-// describe('LinkedList', () => {
-//     it('should be ok', () => {
-//         expect(Value).toBe(100)
-//     })
-// })
-export {}
