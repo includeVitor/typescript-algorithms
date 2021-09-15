218a75ae66b08cd949dd99a55fef1624e5418a17
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-09-14 22:55:42 -0300

    add CustomComparator test case

--- src/comparator/__tests__/Comparator.ts
+++ src/comparator/__tests__/Comparator.ts
@@ -19,5 +19,25 @@ describe('Comparator', () => {
 
         expect(comparator.greaterThan(25, 25)).toBe(false)
         expect(comparator.greaterThan(0, -25)).toBe(true)
+
+        expect(comparator.greaterThanOrEqual(200, 0)).toBe(true)
+        expect(comparator.greaterThanOrEqual(200, 200)).toBe(true)
+        expect(comparator.greaterThanOrEqual(0, 200)).toBe(false)
+    })
+
+    it('should be able to compare with a custom function', () => {
+        const CustomComparator = new Comparator((a: string, b: string) => {
+            if (a.length === b.length) return 0
+            return a.length < b.length ? -1 : 1
+        })
+
+        expect(CustomComparator.equal('X', 'a')).toBe(true)
+        expect(CustomComparator.equal('x', '')).toBe(false)
+        expect(CustomComparator.lessThan('b', 'xx')).toBe(true)
+        expect(CustomComparator.greaterThanOrEqual('s', 'sh')).toBe(false)
+        expect(CustomComparator.greaterThanOrEqual('sX', 'a')).toBe(true)
+        expect(CustomComparator.greaterThanOrEqual('k', 'k')).toBe(true)
+
+        CustomComparator.reverse()
     })
 })
