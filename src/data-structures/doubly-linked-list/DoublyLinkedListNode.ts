3df8496e1350f89f84f38b2e40eed8eaaf041dc8
Vitor Hugo Nunes Da Silva <vitorzcomputacao@gmail.com>
2021-11-17 23:34:26 -0300

    add toString method

--- src/data-structures/doubly-linked-list/DoublyLinkedListNode.ts
+++ src/data-structures/doubly-linked-list/DoublyLinkedListNode.ts
@@ -1,5 +1,12 @@
-import { Node } from './types'
+import {
+    Node,
+    ToStringArguments
+} from '@data-structures/doubly-linked-list/types'
 
 export default class DoublyLinkedListNode<T = never> {
     constructor(public value: T, public next: Node<T> = null) {}
+
+    toString(callback: ToStringArguments = null): string {
+        return callback ? callback(this.value) : `${this.value}`
+    }
 }
--- src/data-structures/doubly-linked-list/types.ts
+++ src/data-structures/doubly-linked-list/types.ts
@@ -2,4 +2,6 @@ import DoublyLinkedListNode from './DoublyLinkedListNode'
 
 type Node<T> = DoublyLinkedListNode<T> | null
 
-export type { Node }
+type ToStringArguments = ((str: any) => string) | null
+
+export type { Node, ToStringArguments }
